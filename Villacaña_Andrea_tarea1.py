3d272b8e7a3eb130f2411762e1a2690eed28fd8d
Andy11-villi <andreavillivazquez@gmail.com>
2025-10-08 23:29:46 -0600

    Add files via upload

---
+++ Villacaña_Andrea_tarea1.py
@@ -0,0 +1,51 @@
+### debes mandar un archivo que pueda ser ejecutado en python, los comentarios deben venir con #
+### 2 faltan todos los incisos
+### 3 faltan los incisos
+### 4 faltan los metodos que pedi
+
+#1 
+Utilizando la función str()
+Ejem: 
+numero= 5
+Texto= “hola“ + str(numero)
+Print(resultado)
+
+#2
+Es clase “Int”, porque es un entero. Integer= “envuelve”
+
+#3
+Palabra= “andrea”
+Print(palabra.upper())
+ANDREA
+
+Palabra= “casa”
+En “Fosa”
+Print( “f” + palabra [1: ] ) 
+
+
+#4
+sirven para realizar operaciones o manipular los datos de ese objeto, encapsulando su comportamiento.
+
+My_string1= “Lindo día”
+My_string2= “para nadar”
+Combined_string= mu_string1 + my string2
+Print(combined_string)
+
+#5
+Qué problema tiene declarar estas variables? (1.5 puntos)
+#  oración larga = 'hola mundo'
+
+#No se permite usar espacios en el nombre de una variable.
+
+#  palabra = 'hola' 'mundo'
+# al hacer esto, python hace...
+#Python concatena automáticamente las palabras.
+
+# 6. Investigar "fstring": qué son, cómo se usan y un ejemplo. 
+# Son una manera de hacer cadenas formateadas de forma rápida y legible.
+# Para usarlos se tiene que poner una letra f antes de las comillas de la cadena, y dentro de llaves {} se escribe la variable o la expresión que quieras mostrar.
+>>> #6   f-strings 
+Nombre= Andrea
+Edad=22
+>>>(“6:”, f=“hola, me llamo {nombre} y tengo { edad} años.”)
+6: “hola me llamo Andrea y tengo 22 años.
